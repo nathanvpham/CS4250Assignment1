74a1485703ffaf5e0937fc7ccd53e45f1b22cc6b
nathanvpham <natepham16@gmail.com>
2023-09-20 20:07:18 -0700

    completed logic in search_engine

--- search_engine.py
+++ search_engine.py
@@ -10,6 +10,7 @@
 
 #importing some Python libraries
 import csv
+import math
 
 documents = []
 labels = []
@@ -68,7 +69,6 @@ print(terms)
 docMatrix = []
 tfMatrix = []
 dfMatrix = []
-idfMatrix =[]
 for doc in documents:
     tfRow = []
     for term in terms:
@@ -85,11 +85,58 @@ for term in terms:
     dfMatrix.append(df)
 print(dfMatrix)
 
+for tfRow in tfMatrix:
+    tfidfRow = []
+    for x in range(len(tfRow)):
+        tfidf = (tfRow[x]) * (math.log(3.0/dfMatrix[x], 10))
+        tfidfRow.append(tfidf)
+    docMatrix.append(tfidfRow)
+print(docMatrix)
+
 
 
 #Calculate the document scores (ranking) using document weigths (tf-idf) calculated before and query weights (binary - have or not the term).
 #--> add your Python code here
 docScores = []
+queryMatrix = []
+for tfRow in tfMatrix:
+    queryRow = []
+    for tf in tfRow:
+        if tf > 0.0:
+            queryRow.append(1.0)
+        else:
+            queryRow.append(0.0)
+    queryMatrix.append(queryRow)
+print(queryMatrix)
+
+for x in range(len(docMatrix)):
+    tfidfRow = docMatrix[x]
+    queryRow = queryMatrix[x]
+    docScore = 0.0
+    for y in range(len(tfidfRow)):
+        docScore = docScore + (tfidfRow[y] * queryRow[y])
+    docScores.append(docScore)
+print(docScores)
+
 
 #Calculate and print the precision and recall of the model by considering that the search engine will return all documents with scores >= 0.1.
 #--> add your Python code here
+docRelevance = []
+for docScore in docScores:
+    if docScore >= 0.1:
+        docRelevance.append(' R')
+    else:
+        docRelevance.append(' I')
+print(docRelevance)
+
+truePositive = 0.0
+falsePositive = 0.0
+for x in range(len(docRelevance)):
+    if docRelevance[x] == labels[x]:
+        truePositive = truePositive + 1.0
+    else:
+        falsePositive = falsePositive + 1.0
+print(labels)
+print(truePositive)
+precision = truePositive/(truePositive+falsePositive)
+print("Precision: " + f"{precision:.2%}")
